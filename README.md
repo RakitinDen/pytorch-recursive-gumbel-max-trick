b9dc09800264697c86cda74b3bc5c82d7539cd4d
RakitinDen <rakitindenis32@gmail.com>
2021-11-19 17:54:30 +0300

    estimators and readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# pytorch-recursive-gumbel-max-trick
+# Leveraging Recursive Gumbel-Max Trick for Approximate Inference in Combinatorial Spaces
+
+This repository contains the PyTorch impementation of main algorithms and how-to-use examples from our paper [Leveraging Recursive Gumbel-Max Trick for Approximate Inference in Combinatorial Spaces](https://arxiv.org/abs/2110.15072).
+
+### Abstract
+
+Structured latent variables allow incorporating meaningful prior knowledge into deep learning models. However, learning with such variables remains challenging because of their discrete nature. Nowadays, the standard learning approach is to define a latent variable as a perturbed algorithm output and to use a differentiable surrogate for training. In general, the surrogate puts additional constraints on the model and inevitably leads to biased gradients. To alleviate these shortcomings, we extend the Gumbel-Max trick to define distributions over structured domains. We avoid the differentiable surrogates by leveraging the score function estimators for optimization. In particular, we highlight a family of recursive algorithms with a common feature we call stochastic invariant. The feature allows us to construct reliable gradient estimates and control variates without additional constraints on the model. In our experiments, we consider various structured latent variable models and achieve results competitive with relaxation-based counterparts.
--- estimators.py
+++ estimators.py
@@ -1,7 +1,12 @@
 import torch
 
-
 def uniform_to_exp(logits, uniform=None, enable_grad=False):
+    '''
+    Converts a tensor of independent uniform samples into a tensor of independent exponential samples
+    Tensor 'logits' contains log-means of the exponential distributions
+    Parameters of the exponentials can be represented as
+    lambda = exp(-logit), since expected value is equal to 1/lambda
+    '''
     if uniform is not None:
         assert uniform.size() == logits.size()
     else:
@@ -13,13 +18,21 @@ def uniform_to_exp(logits, uniform=None, enable_grad=False):
 
     return exp
 
-
 def reattach_exp_to_new_logits(logits, exp):
+    '''
+    Creates a new tensor of exponential variables that depends on logits in the same way
+    as if it was obtained by transforming uniform samples via 'uniform_to_exp'
+    Used in 'relax' to obtain gradient for the detached version of the logits
+    '''
     exp = torch.exp(torch.log(exp.detach()) + logits - logits.detach())
     return exp
 
-
 def E_reinforce(loss_value, logits, exp, plus_samples=1, mask_unused_values=None, **kwargs):
+    '''
+    Returns the REINFORCE [williams1992] gradient estimate with respect to the exponential score
+    grad = loss(X) * (d / d logits) log p(E ; logits)
+    If plus_samples > 1, the estimate is E-REINFORCE+ / E-REINFORCE with LOO baseline [kool2019buy, richter2020vargrad]
+    '''
     batch_size = logits.shape[0] // plus_samples
 
     loss_value = loss_value.detach()
@@ -52,8 +65,12 @@ def E_reinforce(loss_value, logits, exp, plus_samples=1, mask_unused_values=None
 
     return grad
 
-
 def T_reinforce(loss_value, struct_var, logits, f_log_prob, plus_samples=1, **kwargs):
+    '''
+    Returns the REINFORCE [williams1992] gradient estimate with respect to the score function of the execution trace
+    grad = loss(X) * (d / d logits) log p(T ; logits)
+    If plus_samples > 1, the estimate is T-REINFORCE+ / T-REINFORCE with LOO baseline [kool2019buy, richter2020vargrad]
+    '''
     batch_size = logits.shape[0] // plus_samples
 
     loss_value = loss_value.detach()
@@ -79,18 +96,33 @@ def T_reinforce(loss_value, struct_var, logits, f_log_prob, plus_samples=1, **kw
 
     return grad
 
-
 def relax(loss_value, struct_var, logits, exp, critic, f_log_prob, f_cond, uniform=None, **kwargs):
+    '''
+    Returns the RELAX [grathwohl2017backpropagation] gradient estimate
+
+    grad = (loss(X(T)) - c(e_2)) * (d / d logits) log p(T ; logits) - (d / d logits) c(e_2) + (d / d logits) c(e_1)
+
+    e_1 ~ p(E ; logits)     - exponential sample
+    T = T(e_1)              - execution trace of the algorithm
+    X = X(T)                - structured variable, obtained as the output of the algorithm
+    e_2 ~ p(E | T ; logits) - conditional exponential sample
+    c(.)                    - critic (typically, a neural network)
+
+    e_1 and e_2 are sampled using the reparameterization trick ('uniform_to_exp' transformation)
+    (d / d logits) c(e_1) and (d / d logits) c(e_2) are the reparameterization gradients
+
+    In code, exp := e_1, cond_exp := e_2
+    '''
     loss_value = loss_value.detach()
     struct_var = struct_var.detach()
 
     logits = logits.detach().requires_grad_(True)
     exp = reattach_exp_to_new_logits(logits, exp)
 
-    exp_cond = f_cond(struct_var, logits, uniform, **kwargs)
+    cond_exp = f_cond(struct_var, logits, uniform, **kwargs)
 
     baseline_exp = critic(exp)
-    baseline_cond = critic(exp_cond).squeeze()
+    baseline_cond = critic(cond_exp).squeeze()
 
     diff = loss_value - baseline_cond
     log_prob = f_log_prob(struct_var, logits, **kwargs)
@@ -121,7 +153,6 @@ def relax(loss_value, struct_var, logits, exp, critic, f_log_prob, f_cond, unifo
         diff = diff.unsqueeze(-1)
 
     grad = diff * score + d_baseline_exp - d_baseline_cond
-
     assert grad.size() == logits.size()
 
     return grad
